36f67db374bd7ced3b1ba39a3cbc02290cdbaf66
brandon cooper, grinil khanna and sandro mijatovic <brandon+grinil+sandro@>
2017-03-16 14:51:01 -0700

    Add functionality to the maps -- problem was with scope upon exports NEED TO EXPORT AND REQUIRE with js files. Also be aware of callback functions in http.

---
+++ js/brew-interface.js
@@ -0,0 +1,14 @@
+//
+// $(document).ready(function(event) {
+//
+// })
+var BrewMap = require('./../js/brew.js').brewMapModule;
+var InitMap = require('./../js/brew.js').initMap;
+var map = require('./../js/brew.js').map;
+
+
+
+$(document).ready(function() {
+  InitMap();
+
+});
--- js/brew.js
+++ js/brew.js
@@ -1,21 +1,18 @@
-var apiKey = require("./../.env").apiKey;
-
+// var apiKey = require("./../.env").apiKey;
+//
 function BrewMap () {
 
 }
 
+var word = "hello";
 var map;
-
-function displayMap () {
-  map = new google.maps.Map(document.getElementsById('map'), {
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
 }
 
-BrewMap.prototype.route = function(from, to) {
-  $.get("https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&callback=initMap")
-  .then(function(response) {
-
-  });
-}
+exports.brewMapModule = BrewMap;
+exports.initMap = initMap;
+exports.map = map;
